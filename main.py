9f10c6637053779351d870c8f74669fe9232e97a
Mateus-Arauj <mateus9509195091@gmail.com>
2022-07-02 17:29:17 -0300

    Versão final

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests 
 from bs4 import BeautifulSoup
 from tqdm import tqdm 
-
+import pypub
+import os
 
 url = 'https://novelmania.com.br'
 
@@ -9,7 +10,10 @@ print("Insira a url da novel:")
 entrada = input()
 print("Insira o nome da novel:")
 nome_novel = input()
+
 response = requests.get(entrada)
+
+my_first_epub = pypub.Epub(nome_novel)
 
 content = response.content
 soup = BeautifulSoup(content, 'html.parser')
@@ -23,7 +27,12 @@ for i in tqdm(card):
         webscrap = requests.get(url + r['href'])
         soup = BeautifulSoup(webscrap.content, 'html.parser')
         result = soup.find('div',{'class': 'text'})
+        title = result.find('h2')
+        print(title.text)
+        c = pypub.create_chapter_from_string(result,title=title.text)
+        my_first_epub.add_chapter(c)
         text_file = open("livro.txt", "a")
         n = text_file. write(result.text)
         text_file. close()
-    
+
+my_first_epub.create_epub(os.getcwd())
