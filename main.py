2247a547bc8e3526365d5c179eb431e5958a9223
Mateus-Arauj <mateus9509195091@gmail.com>
2022-12-07 15:50:05 -0300

    finalizanod

--- main.py
+++ main.py
@@ -2,33 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm 
 import pypub
-import os
+import re
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+
 
 url = 'https://kiniga.com/'
 
 print("Insira a url da novel:")
-entrada = 'https://kiniga.com/projeto/os-contos-de-anima/'
+entrada = input()
 print("Insira o nome da novel:")
-nome_novel = 'Teste'
+nome_novel = input()
+options = Options()
+# options.headless = True
+driver = webdriver.Firefox(options= options)
+driver.maximize_window()
+driver.get(entrada)
+driver.find_element(By.CLASS_NAME, "btn-reverse-order").click()
+pageSource = driver.page_source
 
-response = requests.get(entrada)
+html = requests.get(entrada).content
 
 my_first_epub = pypub.Epub(nome_novel)
+soup = BeautifulSoup(pageSource, 'html.parser')
+card = soup.find("div", {"id": "manga-chapters-holder"})
+card = soup.find("ul", {"class": "main version-chap active"})
+card = soup.find_all("li", {"class": "wp-manga-chapter"})
 
-content = response.content
-soup = BeautifulSoup(content, 'html.parser')
-card = soup.find('div', {'class': 'listing-chapters_wrap show-more show'})
-print(card)
-# for i in tqdm(card):
-#     print(i)
-#     r = i.find('a')
-#     webscrap = requests.get(r['href'])
-#     soup = BeautifulSoup(webscrap.content, 'html.parser')
-#     print(soup.prettify)
-#     result = soup.find('div',{'class': 'text'})
-#     title = result.find('h2')
-#     print(title.text)
-#     c = pypub.create_chapter_from_string(result,title=title.text)
-#     my_first_epub.add_chapter(c)
+for i in tqdm(card):
+    r = i.find('a')
+    webscrap = requests.get(r['href'])
+    soup = BeautifulSoup(webscrap.content, 'html.parser')
+    result = soup.find('div',{'class': 'text-left'})
+    title = r.text
+    #print(title)
+    #print(result.text)
+    c = pypub.create_chapter_from_string(result,title=title)
+    my_first_epub.add_chapter(c)
 
-# my_first_epub.create_epub(os.getcwd())
+my_first_epub.create_epub(os.getcwd())
