5068e631342b8f06bb05f0179cb3fade4945833d
Mateus-Arauj <mateus9509195091@gmail.com>
2022-12-11 19:09:21 -0300

    Adicionando funções tudo num só

--- main.py
+++ main.py
@@ -8,41 +8,76 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 import os
 
-print("Insira a url da novel:")
-entrada = input()
-print("Insira o nome da novel:")
-nome_novel = input()
-options = Options()
-options.headless = True
+
+def kiniga():
+    print("Insira a url da novel:")
+    entrada = input()
+    print("Insira o nome da novel:")
+    nome_novel = input()
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options= options)
+    driver.maximize_window()
+    driver.get(entrada)
+    driver.find_element(By.CLASS_NAME, "btn-reverse-order").click()
+    pageSource = driver.page_source
+    driver.quit()
+
+    soup = BeautifulSoup(pageSource, 'html.parser')
+    imgUrl = soup.find('div', {'class': 'summary_image'})
+    imgUrl = imgUrl.find('img')['data-src']
+    urllib.request.urlretrieve(imgUrl, f"imgs/{nome_novel}.jpg")
+    card = soup.find("div", {"id": "manga-chapters-holder"})
+    card = soup.find("ul", {"class": "main version-chap active"})
+    card = soup.find_all("li", {"class": "wp-manga-chapter"})
+    my_first_epub = pypub.Epub(nome_novel, cover = f'imgs/{nome_novel}.jpg')
 
 
+    for i in tqdm(card, unit = "Cap"):
+        r = i.find('a')
+        webscrap = requests.get(r['href'])
+        soup = BeautifulSoup(webscrap.content, 'html.parser')
+        result = soup.find('div',{'class': 'text-left'})
+        title = r.text
+        #print(title)
+        #print(result.text)
+        c = pypub.create_chapter_from_string(result,title=title)
+        my_first_epub.add_chapter(c)
+    my_first_epub.create_epub(os.getcwd()+'/ebooks')
 
-driver = webdriver.Firefox(options= options)
-driver.maximize_window()
-driver.get(entrada)
-driver.find_element(By.CLASS_NAME, "btn-reverse-order").click()
-pageSource = driver.page_source
-driver.quit()
+def saikaiscan():
+    url = 'https://saikaiscan.com.br'
+    print("Insira a url da novel:")
+    entrada = 'https://saikaiscan.com.br/series/tales-of-demons-and-gods-tdg?tab=capitulos'
+    print("Insira o nome da novel:")
+    #nome_novel = input()
+    options = Options()
+    #options.headless = True
+    driver = webdriver.Firefox(options= options)
+    driver.maximize_window()
+    driver.get(entrada)
+    #driver.find_element(By.CLASS_NAME, "btn-reverse-order").click()
+    pageSource = driver.page_source
+    my_first_epub = pypub.Epub('teste')
+    volumesAction = driver.find_elements(By.CLASS_NAME, "active")
+    volumes = []
+    for volumeAction in volumesAction:
+        volumeAction.click()
+        volumes.append(driver.find_element(By.CLASS_NAME, "__chapters").get_attribute('innerHTML'))
 
-soup = BeautifulSoup(pageSource, 'html.parser')
-imgUrl = soup.find('div', {'class': 'summary_image'})
-imgUrl = imgUrl.find('img')['data-src']
-urllib.request.urlretrieve(imgUrl, f"imgs/{nome_novel}.jpg")
-card = soup.find("div", {"id": "manga-chapters-holder"})
-card = soup.find("ul", {"class": "main version-chap active"})
-card = soup.find_all("li", {"class": "wp-manga-chapter"})
-my_first_epub = pypub.Epub(nome_novel, cover = f'imgs/{nome_novel}.jpg')
+    for volume in tqdm(volumes, unit = "Vol"):
+        soup = BeautifulSoup(volume, 'html.parser')
+        soup = soup.find_all('li')
+        for li in tqdm(soup, unit = "Cap"):
+            r = li.find('a')
+            webscrap = requests.get(url + r['href'])
+            soup = BeautifulSoup(webscrap.content, 'html.parser')
+            result = soup.find('div',{'class': 'text-left'})
+            title = r.text
+            #print(title)
+            #print(result.text)
+            c = pypub.create_chapter_from_string(result,title=title)
+            my_first_epub.add_chapter(c)
+    driver.quit()
 
-
-for i in tqdm(card, unit = "Cap"):
-    r = i.find('a')
-    webscrap = requests.get(r['href'])
-    soup = BeautifulSoup(webscrap.content, 'html.parser')
-    result = soup.find('div',{'class': 'text-left'})
-    title = r.text
-    #print(title)
-    #print(result.text)
-    c = pypub.create_chapter_from_string(result,title=title)
-    my_first_epub.add_chapter(c)
-
-my_first_epub.create_epub(os.getcwd()+'/ebooks')
+saikaiscan()
